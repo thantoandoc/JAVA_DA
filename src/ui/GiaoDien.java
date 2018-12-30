03586dcdce634ceee6586cd407c85c24b292554b
Gracefully Broken <thantoandoc83@gmail.com>
2018-12-30 15:52:18 +0700

    tiep tuc giao dien

--- src/ui/GiaoDien.java
+++ src/ui/GiaoDien.java
@@ -1,22 +1,34 @@
 package ui;
 
 import java.awt.BorderLayout;
+import java.awt.Insets;
 
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 @SuppressWarnings("serial")
 public class GiaoDien extends JFrame{
 	public static int WIDTH = 720;
 	public static int HEIGHT = 520;
 	
-	private Box box, searchBox, buttonBox;
-	private JLabel searchName;
+	private Box box, searchBox;
 	private JTextField txtSearch;
 	private JButton btnSearch;
+	private DefaultTableModel tableModel;
+	private JTable table;
+	private JComboBox<String> cbb;
+	private String[] arrS = {
+			"Tim Kiem Theo Ten",
+			"Tim Kiem Theo Sach"
+	};
 	
 	public GiaoDien(String title) {
 		super(title);
@@ -27,21 +39,40 @@ public class GiaoDien extends JFrame{
 	}
 	private void mapWidgets() {
 		box = Box.createVerticalBox();
+		box.setBorder(new EmptyBorder(new Insets(16, 16, 16, 16)));
 		searchBox = Box.createHorizontalBox();
-		buttonBox = Box.createHorizontalBox();
-		box.add(searchBox);
-		box.add(Box.createVerticalStrut(16));
-		box.add(buttonBox);
-		box.add(Box.createVerticalStrut(8));
 		
-		this.add(box, BorderLayout.NORTH);
-		
-		searchName = new JLabel("Ten");
+		cbb = new JComboBox<>(arrS);
 		txtSearch = new JTextField(20);
 		btnSearch = new JButton("Tim Kiem");
-		searchBox.add(searchName);
+		
+		searchBox.add(cbb);
 		searchBox.add(txtSearch);	
 		searchBox.add(btnSearch);
+		
+		box.add(searchBox);
+		box.add(Box.createVerticalStrut(16));
+		this.add(box, BorderLayout.NORTH);
+		
+		
+		tableModel = new DefaultTableModel();
+		table = new JTable(tableModel);
+		table.setRowMargin(4);
+		tableModel.addColumn("Ma so");
+		tableModel.addColumn("Ten");
+		tableModel.addColumn("Tac gia");
+
+		tableModel.addColumn("Nha bien soan");
+
+		tableModel.addColumn("Nam phat hanh");
+
+		tableModel.addColumn("Trang thai");
+		tableModel.addColumn("");
+
+		JScrollPane sc = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		this.add(sc , BorderLayout.CENTER);
+		
 	}
 	
 }
